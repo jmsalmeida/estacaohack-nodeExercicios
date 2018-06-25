46b3a0815444cd7d7755c13d27680904d5704a5f
James Almeida <james.almeida.ti@gmail.com>
2018-06-25 19:49:47 -0300

    Adicionar endpoint POST/churro para adicionar novo Churro ao Banco de Dados Mongo e GET/churros para listar todos do DB

--- api-mongodb-connect/index.js
+++ api-mongodb-connect/index.js
@@ -1,6 +1,32 @@
 const express = require(`express`);
 const bodyParser = require(`body-parser`);
 const app = express();
+const expressMongoDb = require('express-mongo-db');
 const port = 3000;
 
+app.use(bodyParser.json());
+app.use(expressMongoDb(`mongodb://localhost/churros`));
+
+app.post('/churro', (req, res) => {
+  req.db.collection('sabores').insert(req.body, error => {
+    if(error){
+      res.send(`Failed to insert data`);
+      return;
+    }
+    res.send(req.body);
+
+  });
+});
+
+app.get('/churros', (req, res) => {
+  req.db.collection('sabores').find().toArray((error, data) => {
+    if(error){
+      res.status(500).send();
+      return;
+    }
+    res.send(data);
+  });
+});
+
 app.listen(port, _ => console.log(`Servidor on porta ${port}`));
+
